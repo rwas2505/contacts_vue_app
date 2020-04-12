cccfc0ced3b290b55f6573f1554435c5cf2a77b1
ryan <ryanwaszak@gmail.com>
2020-04-12 12:32:34 -0500

    add test route

---
+++ src/views/Test.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="test">
+    <h1>{{ message }}</h1>
+    <p>I am on the test page</p>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js! from the test page"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
